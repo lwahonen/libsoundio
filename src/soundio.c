83548c671d22fa291028d5bd26cd74ee05296bd7
L Will Ahonen <lauri.ahonen@iki.fi>
2019-06-11 15:10:17 -0700

    This was a silly idea, skip it

--- src/soundio.c
+++ src/soundio.c
@@ -794,8 +794,6 @@ bool soundio_device_supports_layout(struct SoundIoDevice *device,
 }
 
 bool soundio_device_supports_sample_rate(struct SoundIoDevice *device, int sample_rate) {
-	if (!device->is_raw)
-		return true;
     for (int i = 0; i < device->sample_rate_count; i += 1) {
         struct SoundIoSampleRateRange *range = &device->sample_rates[i];
         if (sample_rate >= range->min && sample_rate <= range->max)
